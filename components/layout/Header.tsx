43ba70f674d6d980f3fe79dba3d47aa7051901a6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-28 16:18:32 +0500

    NAVABR UPDATED

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -26,7 +26,7 @@ export default function AdminLayout() {
 
   return (
     <header className="bg-blue-600 text-white">
-      <div className="max-w-7xl mx-auto flex justify-between items-center p-4">
+      <div className="max-w-7xl mx-auto flex flex-wrap items-center justify-between p-4">
         {/* Logo and Title */}
         <div className="flex items-center space-x-3">
           <Image
@@ -38,26 +38,39 @@ export default function AdminLayout() {
           />
           <h1 className="text-2xl font-bold">Rider Revolution</h1>
         </div>
+        <div className=" flex items-center gap-5">
+          {/* Navigation Links */}
+          <nav className="flex space-x-6 items-center text-sm md:text-base mt-3 md:mt-0">
+            <a href="/dashboard" className="hover:text-gray-300 transition">
+              Bids
+            </a>
+            <a href="/listing" className="hover:text-gray-300 transition">
+              Listing
+            </a>
+          </nav>
 
-        {/* User Info and Sign Out */}
-        {User.email ? (
-          <div className="flex items-center space-x-4">
-            <h1 className="text-lg">{User.name}</h1>
-            <button
-              onClick={handleSignOut}
-              className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
-            >
-              Sign Out
-            </button>
+          {/* User Info and Sign Out/Login */}
+          <div className="flex space-x-4 items-center mt-3 md:mt-0">
+            {User.email ? (
+              <div className="flex flex-col items-center space-x-4">
+                <h1 className="text-sm md:text-lg">{User.name}</h1>
+                <button
+                  onClick={handleSignOut}
+                  className="bg-red-500 text-white px-3 py-2 rounded hover:bg-red-600 transition text-xs md:text-sm"
+                >
+                  Sign Out
+                </button>
+              </div>
+            ) : (
+              <button
+                onClick={() => Router.push('/signin')}
+                className="bg-green-500 text-white px-3 py-2 rounded hover:bg-green-600 transition text-xs md:text-sm"
+              >
+                Login
+              </button>
+            )}
           </div>
-        ) : (
-          <button
-            onClick={() => Router.push('/signin')}
-            className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition"
-          >
-            Login
-          </button>
-        )}
+        </div>
       </div>
     </header>
   )

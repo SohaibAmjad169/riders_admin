fd93fc4a39c1e264f2e15438b9daf9eafe7a42d7
tr-zrafiq <zeeshan.r@turing.com>
2025-01-09 00:44:21 +0500

    change currency

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "15ea36e67797d43e"; }
+/******/ 		__webpack_require__.h = function() { return "c2546b9104cbd172"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
